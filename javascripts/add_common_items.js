fb67109d653da12a20301b19967448be6031f8e1
Gyrth <gyrthmcmulin@gmail.com>
2021-06-22 03:05:14 +0200

    Worked on OG page and social media links.

--- javascripts/add_common_items.js
+++ javascripts/add_common_items.js
@@ -1,21 +1,23 @@
 addHeader();
 addNav();
 addFooter();
-addVideoControls();
+
 $( document ).ready(function(){
 	$(".button-collapse").sideNav();
 })
+
 function addHeader(){
 	document.querySelector ('header').innerHTML =
 	'<h1>Gyrth McMulin</h1>'+
 	'<h3>Programmer.</h3>'+
 	'<hr></hr>';
 }
+
 function addNav() {
 	document.getElementById("navigator").innerHTML =
 	'<nav>'+
 		'<div class="nav-wrapper">'+
-	      '<a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>'+
+		  '<a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>'+
 			'<ul class="left hide-on-med-and-down">'+
 				'<li><a href="index.html">Main Page</a></li>'+
 				'<li><a href="overgrowth.html">Overgrowth</a></li>'+
@@ -24,30 +26,36 @@ function addNav() {
 			'<ul class="right hide-on-med-and-down">'+
 				'<li><a href="https://www.youtube.com/channel/UCS_9m_pTohKCh2kyKfk6xXw"><div class="right soc-med"><img src="images/youtube.png"></div></a></li>'+
 				'<li><a href="https://twitter.com/GyrthMcMulin"><div class="right soc-med"><img src="images/twitter.png"></div></a></li>'+
-	        '</ul>'+
+				'<li><a href="https://www.linkedin.com/in/hendrik-wiersma/"><div class="right soc-med"><img src="images/linkedin.png"></div></a></li>'+
+			'</ul>'+
 			'<ul class="side-nav" id="mobile-demo">'+
 				'<li><a href="index.html">Main Page</a></li>'+
 				'<li><a href="overgrowth.html">Overgrowth</a></li>'+
 				'<li><a href="school_projects.html">School Projects</a></li>'+
 				'<li><a href="https://www.youtube.com/channel/UCS_9m_pTohKCh2kyKfk6xXw">YouTube<div class="right soc-med"><img src="images/youtube.png"></div></a></li>'+
 				'<li><a href="https://twitter.com/GyrthMcMulin">Twitter<div class="right soc-med"><img src="images/twitter.png"></div></a></li>'+
-		    '</ul>'+
+				'<li><a href="https://www.linkedin.com/in/hendrik-wiersma/">LinkedIn<div class="right soc-med"><img src="images/linkedin.png"></div></a></li>'+
+			'</ul>'+
 		'<div>'+
 	'</nav>';
 }
+
 function addFooter(){
 	document.querySelector ('footer').innerHTML =
-	'<p>Made by Gyrth McMulin.</p>'+
-	'<p><small>Hosted on GitHub Pages<br>Theme by <a href="https://github.com/orderedlist">orderedlist</a><br>and <a href="https://materializecss.com/">materializecss</a></small></p>';
+	'<p><small>Hosted on GitHub Pages<br>'+
+	'Theme by <a href="https://github.com/orderedlist">orderedlist</a></small></p>';
 }
-function addVideoControls(){
-	var divs = document.querySelectorAll('video').forEach(function(video) {
-		video.disableRemotePlayback = true;
-		video.addEventListener('click',function(){
-    		if (video.paused)
-            	video.play();
-        	else
-            	video.pause();
-    	},false);
-	});
-}
+
+var myvid = document.getElementById('playlist');
+var player = $('#playlist')
+var videos = player.data('playlist');
+var activeVideo = 0;
+
+myvid.addEventListener('ended', function(e) {
+	// update the new active video index
+	activeVideo = (++activeVideo) % videos.length;
+
+	// update the video source and play
+	myvid.src = videos[activeVideo];
+	myvid.play();
+});
